56cb75fdfdd479841480b043c4e6361222820301
Merveille Jora <88484917+MerveilleJora@users.noreply.github.com>
2022-03-05 18:09:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let TaskBox = document.querySelector('.items')
-let Clearbtn = document.getElementById('clearBtn')
 let input = document.getElementById('input')
 
 function add() {
@@ -7,7 +6,7 @@ function add() {
     if (!input.value) {
         input.style.border = "2px solid red";
     } else {
-        input.style.border = "1px solid black";
+        input.style.border = "1px solid green";
         var task = document.createElement('li')
         TaskBox.appendChild(task)
         task.classList.add('item')
@@ -15,6 +14,9 @@ function add() {
         task.appendChild(text)
         text.classList.add('li-text')
         text.innerText = input.value
+        let removeBtn = document.createElement("span")
+        task.appendChild(removeBtn)
+        removeBtn.innerHTML = '<i class "fas fa-trash" onclick="deleTask(this)"></i>'
         input.value = "";
     }
 }
